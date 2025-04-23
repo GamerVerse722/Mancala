2c75b7b9d7cc3fe0cad7b34fd47c5562f008bbd3
GamerVerse <cristian987654321rios@gmail.com>
2025-04-23 09:18:54 -0500

    fix some edge cases

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ class Board:
         self.player_a: Player = Player("A", [1, 2, 3, 4, 5, 6], 7)
         self.player_b: Player = Player("B", [13, 12, 11, 10, 9, 8], 0)
         self.current_player: Player = self.player_a
+        self._last_captured: bool = False
+        self.was_captured: bool = False
 
     @staticmethod
     def create_board() -> List[int]:
@@ -28,6 +30,7 @@ class Board:
         return True if position in player.holes and self.board[position] != 0 else False
 
     def make_move(self, position: int) -> bool:
+        self.was_captured = False
         beads: int = self.board[position]
         self.board[position] = 0
         index: int = position
@@ -37,12 +40,19 @@ class Board:
             if index == self.get_opponent().mancala_spot:
                 continue
 
+            # capture opponent holes
             if index in self.current_player.holes and self.board[index] == 0 and beads == 1:
                 self.board[self.current_player.mancala_spot] += 1
 
                 flipped_index: int = self.get_opponent().holes[self.current_player.holes.index(index)]
                 self.board[self.current_player.mancala_spot] += self.board[flipped_index]
+                if self.board[flipped_index] != 0: self.was_captured = True
                 self.board[flipped_index] = 0
+
+                if sum(self.board[1:7]) == 0 and self.current_player == self.player_b:
+                    self._last_captured = True
+                elif sum(self.board[8:]) == 0 and self.current_player == self.player_a:
+                    self._last_captured = True
 
                 return False
 
@@ -63,12 +73,15 @@ class Board:
         return self.player_b if self.board[0] > self.board[7] else self.player_a
 
     def tally_score(self) -> None:
+        if self._last_captured: self.swap_players()
         total_points: int = sum(self.board[1:7] + self.board[8:])
 
         for index in range(14):
             if index == self.current_player.mancala_spot: self.board[index] += total_points
             elif index == self.get_opponent().mancala_spot: continue
             else: self.board[index] = 0
+
+        if self._last_captured: self.swap_players()
 
     def swap_players(self) -> None:
         self.current_player = self.get_opponent()
@@ -77,13 +90,13 @@ class Board:
         return self.player_a if self.current_player == self.player_b else self.player_b
 
     def print_board(self) -> None:
-        print("\033[0;107m\033[1;91m" + " " * 8 +self._format_row([x for x in range(13, 7, -1)]))
-        print("-" * 8 * 7)
-        print(self._format_row(["B:"] + self.board[14:7:-1]))
-        print(f"{self.board[0]:^8}{' ' * 8 * 5}\033[1;94m{self.board[7]:^8}")
-        print(self._format_row(["A:"] + self.board[1:7]))
-        print("-" * 8 * 7)
-        print(" " * 8 + self._format_row([x for x in range(1, 7)]) + "\033[0m")
+        print("\033[0;107m\033[1;91m" + " " * 8 + self._format_row([x for x in range(13, 7, -1)]) + "\033[0m")
+        print("\033[0;107m\033[1;91m" + "-" * 8 * 7 + "\033[0m")
+        print("\033[0;107m\033[1;91m" + self._format_row(["B:"] + self.board[14:7:-1]) + "\033[0m")
+        print("\033[0;107m\033[1;91m" + f"{self.board[0]:^8}{' ' * 8 * 5}\033[1;94m{self.board[7]:^8}" + "\033[0m")
+        print("\033[0;107m\033[1;94m" + self._format_row(["A:"] + self.board[1:7]) + "\033[0m")
+        print("\033[0;107m\033[1;94m" + "-" * 8 * 7 + "\033[0m")
+        print("\033[0;107m\033[1;94m" + " " * 8 + self._format_row([x for x in range(1, 7)]) + "\033[0m")
 
     @staticmethod
     def _format_row(row: List[Any]) -> str:
@@ -91,9 +104,13 @@ class Board:
 
 board = Board()
 
+board.board = [0, 1, 0, 0, 4, 0, 0, 0, 2, 0, 0, 6, 4, 4]
+# board.swap_players()
+
 while True:
     os.system('clear')
     board.print_board()
+    if board.was_captured: print(f"\033[1;93mPlayer {board.current_player.name} captured opponent's beads!\033[0m")
     print(f"Players {board.current_player.name}'s Turn!")
     print(f"Possible moves: {board.current_player.possible_holes()}")
     while True:
@@ -112,5 +129,6 @@ while True:
         board.tally_score()
         os.system('clear')
         board.print_board()
+        if board.was_captured: print(f"\033[1;93mPlayer {board.current_player.name} captured opponent's beads!\033[0m")
         print("Draw!") if board.get_winner() is None else print(f"Player {board.get_winner().name} wins!")
         break
